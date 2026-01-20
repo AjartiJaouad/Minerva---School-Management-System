7f872ba310cef82040cfb6446b75a29e833ad2f4
ajartijaouad <ajartijaouad14@gmail.com>
2026-01-20 17:43:11 +0100

    update class user

--- scholl-app/app/Models/User.php
+++ scholl-app/app/Models/User.php
@@ -1,29 +1,31 @@
 <?php
-
-namespace App\Mosels;
-
-use APP\Core\Database;
+namespace App\Models; 
+use App\Core\Database; 
 use PDO;
 
 class User
 {
-   private $db;
-   public function __construct()
-   {
+    private $db;
 
-      $this->db = Database::getInstance()->getConnction();
-   }
-   public function findByEmail($email)
-   {
-      $stmt = $this->db->prepare("SELECT * FROM users  WHERE email = :email LIMIT 1");
-      $stmt->execute(['email => $email']);
-      return $stmt->fetch();
-   }
-   public function create($name, $email, $password, $role)
-   {
-      $hash = password_hash($password, PASSWORD_DEFAULT);
-      $sql = "INSERT INTO users (name ,email ,password ,role ) VALUES (?,?,?,?)";
-      $stmt = $this->db->prepare($sql);
-      return $stmt->execute([$name, $email, $hash, $role]);
-   }
+    public function __construct()
+    {
+        $this->db = Database::getInstance()->getConnection();
+    }
+
+    public function findByEmail($email)
+    {
+        $stmt = $this->db->prepare("SELECT * FROM users WHERE email = :email LIMIT 1");
+        
+        $stmt->execute(['email' => $email]); 
+        
+        return $stmt->fetch();
+    }
+
+    public function create($name, $email, $password, $role)
+    {
+        $hash = password_hash($password, PASSWORD_DEFAULT);
+        $sql = "INSERT INTO users (name, email, password, role) VALUES (?, ?, ?, ?)";
+        $stmt = $this->db->prepare($sql);
+        return $stmt->execute([$name, $email, $hash, $role]);
+    }
 }
