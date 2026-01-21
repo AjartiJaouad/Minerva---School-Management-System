ba6d5d8a2cb3196e09be27c812f6ee0ab47fedf8
ajartijaouad <ajartijaouad14@gmail.com>
2026-01-21 16:28:52 +0100

    feat: Add create method to ClassController

--- scholl-app/app/Controllers/ClassController.php
+++ scholl-app/app/Controllers/ClassController.php
@@ -1,15 +1,30 @@
 <?php
+
 namespace App\Controllers;
 
 use App\Models\Classe;
 
-class ClassController {
-    
-    public function index() {
+class ClassController
+{
+
+    public function index()
+    {
         $classeModel = new Classe();
         $classes = $classeModel->getAll();
 
         require_once dirname(__DIR__) . '/views/classes/index.php';
     }
-
+    public function create()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $name = $_POST['name'];
+            $teacher_id = $_POST['teacher_id'];
+            $classeModel = new Classe();
+            if ($classeModel->create($name, $teacher_id)) {
+                header('Location: /classes');
+                exit;
+            }
+        }
+        require_once dirname(__DIR__) . '/views/classes/create.php';
+    }
 }
