c871169954052277ce03b49e478ebe5ae32446f7
ajartijaouad <ajartijaouad14@gmail.com>
2026-01-21 16:06:03 +0100

    feat: Add create method to Classe model

--- scholl-app/app/Models/Classe.php
+++ scholl-app/app/Models/Classe.php
@@ -19,15 +19,33 @@ class Classe
         $db = new Database();
         $this->conn = $db->connect();
     }
-   // en recuprere tout les  classes avec le nom de l'enseignant
-   public function getAll() {
+    // en recuprere tout les  classes avec le nom de l'enseignant
+    public function getAll()
+    {
         $query = "SELECT classes.*, users.username as teacher_name 
                   FROM " . $this->table . "
                   LEFT JOIN users ON classes.teacher_id = users.id
                   ORDER BY classes.created_at DESC";
-                  
+
         $stmt = $this->conn->prepare($query);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    public function create($name, $teacher_id)
+    {
+        $query = "INSERT INTO " . $this->table . " (name, teacher_id) VALUES (:name, :teacher_id)";
+        $stmt = $this->conn->prepare($query);
+
+        // pour la Security
+        $name = htmlspecialchars(strip_tags($name));
+        $teacher_id = htmlspecialchars(strip_tags($teacher_id));
+
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':teacher_id', $teacher_id);
+
+        if ($stmt->execute()) {
+            return true;
+        }
+        return false;
     }
 }
