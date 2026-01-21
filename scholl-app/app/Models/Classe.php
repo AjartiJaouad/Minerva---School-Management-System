a4137e55c896d662b9217aa56af7dfdb03ac8e42
ajartijaouad <ajartijaouad14@gmail.com>
2026-01-21 16:03:21 +0100

    feat: Add getAll method to Classe model

--- scholl-app/app/Models/Classe.php
+++ scholl-app/app/Models/Classe.php
@@ -19,5 +19,15 @@ class Classe
         $db = new Database();
         $this->conn = $db->connect();
     }
-   
+   // en recuprere tout les  classes avec le nom de l'enseignant
+   public function getAll() {
+        $query = "SELECT classes.*, users.username as teacher_name 
+                  FROM " . $this->table . "
+                  LEFT JOIN users ON classes.teacher_id = users.id
+                  ORDER BY classes.created_at DESC";
+                  
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
