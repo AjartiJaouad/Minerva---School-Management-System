1889f1da9887fba6cdf0d0d8e8698bca95f3cd8f
bilalzarrik01 <bilalzarrik11@gmail.com>
2026-01-23 16:38:18 +0100

    delelte

--- scholl-app/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-// db.php - Database configuration
-
-define('DB_HOST', 'localhost');
-define('DB_PORT', '3306');       // Custom port
-define('DB_NAME', 'school_app');
-define('DB_USER', 'root');
-define('DB_PASS', 'azer1234');
-
